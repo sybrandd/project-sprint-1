cfd8d11b73b8e48c4ca4829cd12ac5d4d4a2dc4f
sybrandd <sybrand.j.adema@gmail.com>
2024-09-10 10:25:17 +0200

    jkjko

---
+++ main.js
@@ -0,0 +1,19 @@
+//Opdrachten variabelen
+
+const userName = "Sybrand";
+let tamprature = 20;
+    console.log(userName);
+    alert(`de temparatuur van vandaag is ${tamprature} graden`);
+    tamprature += 5
+        console.log (tamprature);
+
+//opdrachten ineractie
+
+const askName = prompt(`wat is je naam`);
+alert(askName);
+
+const cookies = confirm(`Accepteer je dat we je cookies mogen bewaren?`);
+console.log(cookies);
+
+const myCity = 'Rijswijk';
+alert('Ik woon in ' + myCity);
